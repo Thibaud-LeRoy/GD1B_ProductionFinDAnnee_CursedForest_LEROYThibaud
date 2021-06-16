5c7fd04813f06cdcd11c6f1fb443c958aa3d57d0
thibaud-leroy <thibaud.leroy98@gmail.com>
2021-06-16 16:40:34 +0200

    maps

--- _code/_levels/sprite_props.tsx
+++ _code/_levels/sprite_props.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="sprite_props" tilewidth="64" tileheight="64" tilecount="220" columns="20">
+<tileset version="1.5" tiledversion="2021.02.15" name="sprite_props" tilewidth="64" tileheight="64" tilecount="220" columns="20">
  <image source="../../_assets/_export/_tileset/sprite_props.png" width="1280" height="720"/>
 </tileset>
